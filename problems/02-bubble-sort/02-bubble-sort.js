0af4523fa0d7e91c6664c66aaf2263d223a5ca56
Kenneth Davis <kennethdavis391@gmail.com>
2019-11-13 13:04:22 -0500

    1-2 complete

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -3,5 +3,22 @@ Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
 function bubbleSort(numArray) {
-  
+    //loop through a number of times equal to the array length
+    for(let i = 0; i < numArray.length; i++){
+        let max = numArray[0]
+        let maxIndex = 0
+        //loop through until you get to the numbers that are already sorted
+        for(let j = 0; j < numArray.length - i; j++){
+            //find the bigest value in the unsorted array
+            if(numArray[j] > max){
+                max = numArray[j]
+                maxIndex = j
+            }
+        }
+        //swap the max with the last index spot
+        let temp = max
+        numArray[maxIndex] = numArray[numArray.length - 1 - i]
+        numArray[numArray.length - 1 - i] = temp
+    }
+    return numArray
 }
