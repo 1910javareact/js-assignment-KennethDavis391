9e80beb154d3dd34f20251475f5261cc576af305
Kenneth Davis <kennethdavis391@gmail.com>
2019-11-13 19:56:55 -0500

    1-7 complete and pure

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,13 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    newStr = someStr.toLowerCase()
+    //loop through the front half and make sure it's equal to the second half
+    for(let i = 0; i < newStr.length/2; i++){
+        if(newStr.charAt(i) !== newStr.charAt(newStr.length - 1 - i)){
+            return false
+        }
+    }
+    //if you get past the for loop without returning false then it must be a palindrome
+    return true
 }
