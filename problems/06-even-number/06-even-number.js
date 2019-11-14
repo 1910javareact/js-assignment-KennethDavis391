ced136c6d5ba1f558ffd54c7d968c00b4723d92c
Kenneth Davis <kennethdavis391@gmail.com>
2019-11-13 19:47:16 -0500

    1-6 complete and pure

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,10 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    //check if it can divide evenly into 2, if not its odd
+    if(Number.isInteger(someNum/2)){
+        return true
+    }else{
+        return false
+    }
 }
