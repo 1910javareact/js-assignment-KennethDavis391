115555cd0cb231fe7224eb19d9d73fe348de6e3f
Kenneth Davis <kennethdavis391@gmail.com>
2019-11-13 21:32:12 -0500

    1-9 complete and pure

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,7 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-    
+    for (const key in someObj) {
+        console.log(`${key}: ${someObj[key]}`);
+    }
 }
